1f8460c1a4c97f3716f02954e7bcd491908bff23
Opilane TTHK <opilane@tthk.ee>
2020-11-20 11:40:00 +0200

    new page

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
 const addButton = document.getElementById('add');
-
-
-
+const foodName = document.querySelector("#item-name");
+const foodCalories = document.querySelector("#item-calories");
+const totalSpan = document.querySelector(".total-calories");
+const list = document.querySelector("#item-list");
+let total = 0;
 
 
 addButton.addEventListener('click', (event)=> {
-    console.log();
+  let mealName = foodName.value; //get the food name from the input field
+  let mealCalories = parseInt(foodCalories.value);//get the calories from t..
+  total = total + mealCalories;//calculates the tottal calorie intake
+  totalSpan.innerHTML = total;
+
+
+let listItem = document.createElement("li");
+listItem.classList.add("collection-item");
+let text = `food: ${mealName}.Calories: ${mealCalories}`;
+listItem.appendChild(document.createTextNode(text));
+console.log(listItem);
+list.appendChild(listItem);
+
+
+  //clear the input fields
+  foodName.value = "";
+  foodCalories.value ="";
+
+
+  console.log("Food:", typeof(mealName));
+  console.log("Calories:", typeof(mealCalories));
+  console.log("Total:", total);
 
     event.preventDefault();
 });
